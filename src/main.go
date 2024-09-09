e7fceb42b9cb540e52b09967e44489e40a36c4cf
Oleksandr Korobeinikov <oleksandr.korobeinikov@gasbi.io>
2024-09-09 15:43:33 +0300

    add http service;

--- src/main.go
+++ src/main.go
@@ -2,9 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/sirupsen/logrus"
 )
+
+// Handler function for the "/hello" endpoint
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Log the request
+	log.Println("Request:", r.Method, r.URL.Path)
+
+	// Respond with "Hello, World!"
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello, World!")
+	log.Println("Response: 'Hello, World'")
+}
 
 func main() {
 	log := logrus.New()
@@ -14,8 +27,12 @@ func main() {
 
 	log.Info("Starting the application...")
 
-	// Print Hello, World! to the console
-	fmt.Println("Hello, World!")
+	http.HandleFunc("/hello-world", helloHandler)
+
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 
 	log.Info("Application finished.")
 }
